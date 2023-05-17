b4a0c60a2f3d5e1a5331124614d0c7d4bde1344f
Yuri Santana <yurisantana@discente.ufg.br>
2023-05-17 18:08:30 -0300

    ex6b

--- lista01B/ex6.c
+++ lista01B/ex6.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 int main () {
-    int n, i , j, k;
-    scanf ("%d", &n);
+    int n, i , k = 1, s = 0, x = 0;
+    do {
+        scanf ("%d", &n);
+    }while (n <= 0);
     int v[n];
     for (i = 0; i < n ; i++) {
         scanf("%d", &v[i]);
     }
     for (i = 0; i < n ; i++) {
-        if (v[i] > v[i + 1]) {
-            k = 0;
-        }
-        else if (v[i] < v[i + 1]) {
+        if (v[i] <= v[i + 1]) {
             k += 1;
+            if (k > s){
+                s = k;
+            }
         }
+        else {
+            k = 1;
+        }
+    
     }
 
-    printf("o comprimento do segmento crescente maximo e: %d", k);
+    printf("O comprimento do segmento crescente maximo e:   %d", s);
     return 0;
 }
