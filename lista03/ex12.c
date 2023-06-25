118d63f6b307cff4dd2bcefd9f5ad09cee488831
Yuri Buenos Aires Santana <yuri.santana@discente.ufg.br>
2023-06-25 00:18:51 -0300

    .

--- lista03/ex12.c
+++ lista03/ex12.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 #define MAX 500
 
-void RetornaUniversidadeLetra(char universidade[],int x) {
-    int j;
-    for (j = 0; j < x; j++) {
-        printf("%c",universidade[j]);
-    }
-    printf("\n");
-}
-
-
 int main() {
-    int n, i;
+    int n, i, j;
     int x;
     scanf("%d",&n);
     char universidade[MAX];
     getchar();
-    for (i = 0 ; i < n ; i++) {
-        scanf("%d%*c%[^\n]%*c",&x ,universidade);
-        RetornaUniversidadeLetra(universidade,x);
-    }
+        for (i = 0; i < n; i++) {
+            scanf("%d%*c%[^\n]%*c",&x ,universidade);
+            for (j = 0; j < x ; j++) {
+                printf("%c",universidade[j]);
+            }
+        }
     return 0;
-}
+    }
